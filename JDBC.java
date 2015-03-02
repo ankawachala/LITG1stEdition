9c828cdbb4a13a911b35c56938fd4da49e57b9e7
ankawachala <anka.wachala@gmail.com>
2015-03-02 23:50:38 +0100

    nothing special

--- JDBC.java
+++ JDBC.java
@@ -11,10 +11,13 @@ public class JDBC {
 		   //  Database credentials
 		   final String USER = "root";
 		   final String PASS = "";
+
+
+		   
 		   
 		   public void createDB() {
-		   Connection conn = null;
-		   Statement stmt = null;
+			   Connection conn = null;
+			   Statement stmt = null;
 		   try{
 		      //STEP 2: Register JDBC driver
 		      Class.forName("com.mysql.jdbc.Driver");
@@ -27,7 +30,7 @@ public class JDBC {
 		      System.out.println("Creating database...");
 		      stmt = conn.createStatement();
 		      
-		      String sql = "CREATE DATABASE STUDENTS";
+		      String sql = "CREATE DATABASE IF NOT EXISTS STUDENTS";
 		      stmt.executeUpdate(sql);
 		      System.out.println("Database created successfully...");
 		   }catch(SQLException se){
@@ -53,4 +56,166 @@ public class JDBC {
 		   System.out.println("Goodbye!");
 		}//end main
 
+		   
+		   
+		   public void createTable() {
+			   Connection conn = null;
+			   Statement stmt = null;
+			      try{
+			         //STEP 2: Register JDBC driver
+			         Class.forName("com.mysql.jdbc.Driver");
+
+			   final String DB_URL2 = "jdbc:mysql://localhost/STUDENTS";
+
+			         //STEP 3: Open a connection
+			         System.out.println("Connecting to a selected database...");
+			         conn = DriverManager.getConnection(DB_URL2, USER, PASS);
+			         System.out.println("Connected database successfully...");
+			         
+			         //STEP 4: Execute a query
+			         System.out.println("Creating table in given database...");
+			         stmt = conn.createStatement();
+			         
+			         String sql = "CREATE TABLE IF NOT EXISTS REGISTRATION" +
+			                      "(id INTEGER not NULL, " +
+			                      " first VARCHAR(255), " + 
+			                      " last VARCHAR(255), " + 
+			                      " age INTEGER, " + 
+			                      " PRIMARY KEY ( id ))"; 
+
+			         stmt.executeUpdate(sql);
+			         System.out.println("Created table in given database...");
+			      }catch(SQLException se){
+			         //Handle errors for JDBC
+			         se.printStackTrace();
+			      }catch(Exception e){
+			         //Handle errors for Class.forName
+			         e.printStackTrace();
+			      }finally{
+			         //finally block used to close resources
+			         try{
+			            if(stmt!=null)
+			               conn.close();
+			         }catch(SQLException se){
+			         }// do nothing
+			         try{
+			            if(conn!=null)
+			               conn.close();
+			         }catch(SQLException se){
+			            se.printStackTrace();
+			         }//end finally try
+			      }//end try
+			      System.out.println("Goodbye!");
+			   }//end main
+		   
+		  
+		   
+		   public void insertRecord(String a, String b, String c, String d) {
+			   Connection conn = null;
+			   Statement stmt = null;
+			   try{
+			      //STEP 2: Register JDBC driver
+			      Class.forName("com.mysql.jdbc.Driver");
+			      
+			      
+			      //STEP 3: Open a connection
+			      System.out.println("Connecting to a selected database...");
+			      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			      System.out.println("Connected database successfully...");
+			      
+			      //STEP 4: Execute a query
+			      System.out.println("Inserting records into the table...");
+			      stmt = conn.createStatement();
+			      
+			   
+			      
+			      String sql = "INSERT INTO Registration " +
+			                   "VALUES ('a', 'b', 'c', 'd')";
+			      stmt.executeUpdate(sql);
+
+			      
+			      
+			      System.out.println("Inserted records into the table...");
+
+			   }catch(SQLException se){
+			      //Handle errors for JDBC
+			      se.printStackTrace();
+			   }catch(Exception e){
+			      //Handle errors for Class.forName
+			      e.printStackTrace();
+			   }finally{
+			      //finally block used to close resources
+			      try{
+			         if(stmt!=null)
+			            conn.close();
+			      }catch(SQLException se){
+			      }// do nothing
+			      try{
+			         if(conn!=null)
+			            conn.close();
+			      }catch(SQLException se){
+			         se.printStackTrace();
+			      }//end finally try
+			   }//end try
+			   System.out.println("Goodbye!");
+			}//end main
+		   
+		   
+		   
+		   
+		   public void selectRecord(){
+			   Connection conn = null;
+			   Statement stmt = null;
+		   try{
+		      //STEP 2: Register JDBC driver
+		      Class.forName("com.mysql.jdbc.Driver");
+
+		      //STEP 3: Open a connection
+		      System.out.println("Connecting to a selected database...");
+		      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		      System.out.println("Connected database successfully...");
+		      
+		      //STEP 4: Execute a query
+		      System.out.println("Creating statement...");
+		      stmt = conn.createStatement();
+
+		      String sql = "SELECT id, first, last, age FROM Registration";
+		      ResultSet rs = stmt.executeQuery(sql);
+		      //STEP 5: Extract data from result set
+		      while(rs.next()){
+		         //Retrieve by column name
+		         int id  = rs.getInt("id");
+		         int age = rs.getInt("age");
+		         String first = rs.getString("first");
+		         String last = rs.getString("last");
+
+		         //Display values
+		         System.out.print("ID: " + id);
+		         System.out.print(", Age: " + age);
+		         System.out.print(", First: " + first);
+		         System.out.println(", Last: " + last);
+		      }
+		      rs.close();
+		   }catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }finally{
+		      //finally block used to close resources
+		      try{
+		         if(stmt!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		      }// do nothing
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		      }//end finally try
+		   }//end try
+		   System.out.println("Goodbye!");
+		}//end main
 }
